c210de6b642f894c6277ffc6379c3f4018e317ee
Splinglove <make_zone@sina.com>
2014-01-21 17:38:32 +0800

    修改文件名问题;

--- small-web/src/main/java/cn/runnerup/actions/commons/DownAction.java
+++ small-web/src/main/java/cn/runnerup/actions/commons/DownAction.java
@@ -1,16 +1,26 @@
 package cn.runnerup.actions.commons;
 
 import java.io.FileInputStream;
-import java.io.OutputStream;
+import java.io.InputStream;
 
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import cn.runnerup.actions.RunnerSupport;
 import cn.runnerup.model.Attachment;
 import cn.runnerup.service.AttachmentService;
+import cn.runnerup.service.MimeService;
 
+@Results({
+	@Result(name = "attachment", type = "stream", params = {
+			"inputName",
+			"inputStream",
+			"allowCaching",
+			"true"
+	})
+})
 public class DownAction extends RunnerSupport {
 
 	private static final long serialVersionUID = -4367629776729927085L;
@@ -20,19 +30,24 @@ public class DownAction extends RunnerSupport {
 	@Autowired
 	private AttachmentService attachmentService;
 
+	@Autowired
+	private MimeService mimeService;
+
 	private Attachment attachment;
+
+	private String contentType;
+
+	private String contentDisposition;
+
+	private InputStream inputStream;
 
 	public String show() throws Exception {
 		if(attachment != null) {
-			servletResponse.setContentType(attachment.getMime());
-			servletResponse.setHeader(
-					"Content-disposition",
-					String.format("attachment; filename=\"%s\"", encodeHttpHeader(attachment.getFilename(), "UTF-8")));
-			OutputStream out = servletResponse.getOutputStream();
-			FileInputStream stream = new FileInputStream(FilenameUtils.concat(attachmentService.getRoot(), attachment.getPath()));
-			IOUtils.copy(stream, out);
+			contentType = mimeService.getMime(attachment.getFilename());
+			contentDisposition = String.format("attachment; filename=\"%s\"", encodeHttpHeader(attachment.getFilename(), "UTF-8"));
+			inputStream = new FileInputStream(FilenameUtils.concat(attachmentService.getRoot(), attachment.getPath()));
 		}
-		return null;
+		return "attachment";
 	}
 
 	public Integer getId() {
@@ -45,6 +60,30 @@ public class DownAction extends RunnerSupport {
 		else
 			addActionError("error");
 		this.id = id;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public String getContentDisposition() {
+		return contentDisposition;
+	}
+
+	public void setContentDisposition(String contentDisposition) {
+		this.contentDisposition = contentDisposition;
+	}
+
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
+	public void setInputStream(InputStream inputStream) {
+		this.inputStream = inputStream;
 	}
 
 }
