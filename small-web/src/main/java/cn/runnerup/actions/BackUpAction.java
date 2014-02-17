b8032d0e39ac9a05cfd565a995253a644f97c6d2
Splinglove <make_zone@sina.com>
2014-02-17 23:59:06 +0800

    boss权限后台控制;

--- small-web/src/main/java/cn/runnerup/actions/BackUpAction.java
+++ small-web/src/main/java/cn/runnerup/actions/BackUpAction.java
@@ -25,13 +25,15 @@ public class BackUpAction extends RunnerSupport implements ModelDriven<BackUpMod
 	
 	public String index() {
 		try {
-			FtpService ftpService = new FtpService(model.getUsername(), model.getPassword(), model.getHostip(), 21);
-			if(!isFile) {
-				Runtime.getRuntime().exec("cmd /c start d:\\files\\small.bat");
-				ftpService.uploadFiles(attachmentService.getRoot(), "");
-			}else
-				ftpService.uploadFiles(FilenameUtils.concat(attachmentService.getRoot(), "small.db"), "");
-			model.setSuccess(true);
+			if(getUser().getPriority() == 5) {
+				FtpService ftpService = new FtpService(model.getUsername(), model.getPassword(), model.getHostip(), 21);
+				if(!isFile) {
+					Runtime.getRuntime().exec("cmd /c start d:\\files\\small.bat");
+					ftpService.uploadFiles(attachmentService.getRoot(), "");
+				}else
+					ftpService.uploadFiles(FilenameUtils.concat(attachmentService.getRoot(), "small.db"), "");
+				model.setSuccess(true);
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
